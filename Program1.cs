87a58da4bb3910263f59d8757fcbbb93232b5b8f
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-04 13:04:57 +0530

    New file added.

---
+++ Program1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayushi_BD
+{
+    internal class Program1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number 1 : ");
+            int n1 = Convert.ToInt32(Console.ReadLine());   
+
+            Console.WriteLine("Enter the number 2 : ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sum of two numbers is : " + (n1 + n2));
+        }
+    }
+}
